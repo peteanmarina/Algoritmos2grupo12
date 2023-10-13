80d22847ae3e7fff320248bac60ec4474a8d606c
nicorf12 <nromano@fi.uba.ar>
2023-10-13 13:57:14 -0300

    tp1 con main mas liviano y menos complejidad

--- tp1/utilidades/busqueda_ordenamiento.go
+++ tp1/utilidades/busqueda_ordenamiento.go
@@ -2,11 +2,70 @@ package utilidades
 
 const NO_ENCONTRADO = -1
 
+func IngresoBinario(elem int, array []int) []int {
+	indice := buscar_ultimo_dni(elem, array)
+	if indice == -1 {
+		return append(array, elem)
+	}
+
+	retornado := make([]int, 0)
+	retornado = append(retornado, array[:indice]...)
+	retornado = append(retornado, elem)
+	retornado = append(retornado, array[indice:]...)
+	return retornado
+}
+
+func QuitaBinaria(elem int, array []int) []int {
+	indice := BusquedaBinaria(array, 0, len(array), elem)
+
+	if indice == NO_ENCONTRADO {
+		return array
+	}
+
+	retornado := make([]int, 0)
+
+	if indice == 0 {
+		der := array[indice+1:]
+		retornado = append(retornado, der...)
+	} else if indice == len(array)-1 {
+		izq := array[:indice]
+		retornado = append(retornado, izq...)
+	} else {
+		izq := array[:indice]
+		der := array[indice+1:]
+		retornado = append(retornado, izq...)
+		retornado = append(retornado, der...)
+	}
+
+	return retornado
+}
+
+func buscar_ultimo_dni(elem int, array []int) int {
+	izq, der := 0, len(array)-1
+
+	for izq <= der {
+		medio := izq + (der-izq)/2
+
+		if array[medio] == elem {
+			return medio
+		} else if array[medio] < elem {
+			izq = medio + 1
+		} else {
+			der = medio - 1
+		}
+	}
+
+	return izq
+}
+
 func BusquedaBinaria(arreglo []int, inicio, fin, elemento int) int {
-	if inicio > fin {
+
+	medio := (inicio + fin) / 2
+
+	if inicio > fin || medio >= len(arreglo) || medio < 0 {
 		return NO_ENCONTRADO
 	}
-	medio := (inicio + fin) / 2
+
 	if arreglo[medio] == elemento {
 		return medio
 	} else if arreglo[medio] < elemento {
